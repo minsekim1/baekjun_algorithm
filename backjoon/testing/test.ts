75b2a7b634296d14579689e06d00262613db85f8
Mins97 <tkarnrwl7862@naver.com>
2022-05-10 09:06:54 +0900

    datilay backup

--- backjoon/testing/test.ts
+++ backjoon/testing/test.ts
@@ -4,16 +4,15 @@ const inputList: string[] = fs.readFileSync("./dev/stdin").toString().trim().spl
 // const input: string = fs.readFileSync("./dev/stdin").toString().trim();
 // const inputN: number = +fs.readFileSync("./dev/stdin").toString().trim();
 
+const dp: number[][] = [[Number(inputList[0][0])]];
 const input = inputList.slice(2).map((item) => item.split(" ").map(Number));
-let resultList: number[] = [Number(inputList[1][0])];
-input.map((row, i) => {
-  const temp: number[] = [];
-  row.map((item, j) => {
-    if (j == resultList.length) temp[j] = resultList[resultList.length - 1] + item;
-    else if (j == 0) temp[j] = resultList[j] + item;
-    else temp[j] = Math.max(resultList[j - 1], resultList[j]) + item;
-  });
-  resultList = temp;
-  // console.log(temp.join(" "));
+
+input.map((item, i) => {
+  console.log(item);
+
+  // item.map((item, j) => {
+  //   dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j + 1]);
+  // });
 });
-console.log(Math.max(...resultList));
+
+console.log(dp);
