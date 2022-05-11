3ab22ecbe6d8b57e94f4ca130ec8787e54acbc4a
Mins97 <tkarnrwl7862@naver.com>
2022-05-11 13:03:42 +0900

    datilay backup

--- backjoon/testing/test.ts
+++ backjoon/testing/test.ts
@@ -2,11 +2,11 @@ const fs = require("fs");
 const inputList: string[] = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 // const inputRow: string[] = fs.readFileSync("./dev/stdin").toString().split(" ");
 // const input: string = fs.readFileSync("./dev/stdin").toString().trim();
-// const inputN: number = +fs.readFileSync("./dev/stdin").toString().trim();
+const inputN: number = +fs.readFileSync("./dev/stdin").toString().trim();
 
-const dp: number[] = inputList.slice(1).map(Number);
-const stair: number[] = new Array(...dp);
+const dp: number[] = [];
 
-dp.map((item, i) => (i < 3 ? null : (dp[i] = Math.max(dp[i - 2] + stair[i], stair[i - 1] + stair[i] + dp[i - 3]))));
-
-console.log(dp, stair);
+let idx = 1;
+for (let i = 0; i < inputN; i++){
+  dp[i] = idx;
+}
