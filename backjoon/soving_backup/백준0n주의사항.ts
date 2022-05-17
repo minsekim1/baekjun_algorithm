d2f4374600d6e74a553e53b5cbad12dad3b897c8
minsekim1 <tkarnrwl7862@naver.com>
2022-05-17 23:30:20 +0900

    datilay backup

---
+++ backjoon/soving_backup/백준0n주의사항.ts
@@ -0,0 +1,2 @@
+// let min = 0n; //컴파일 에러남
+// let min: bigint = BigInt(0); //이걸로해야함
--- backjoon/testing/test.ts
+++ backjoon/testing/test.ts
@@ -1,6 +1,35 @@
 const fs = require("fs");
-// const inputList: string[] = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
-// const input: number = +fs.readFileSync("./dev/stdin").toString().trim();
-const input: string = fs.readFileSync("./dev/stdin").toString().trim();
+const inputList: string[] = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
-console.log(input);
+let map = new Map();
+
+const getParent = (n: string): string => {
+  if (n === map.get(n)) return n;
+  const p = getParent(map.get(n));
+  map.set(n, p);
+  return p;
+};
+
+const unionParent = (n1: string, n2: string) => {
+  const n11 = getParent(n1);
+  // const n22 = getParent(n2);
+  map.set(n2, n11);
+};
+
+const equalParent = (n1: string, n2: string) => getParent(n1) === getParent(n2);
+
+let result = "";
+inputList.map((item) => {
+  if (item.indexOf(" ") === -1) {
+    map = new Map();
+    return;
+  }
+  const [n1, n2] = item.split(" ");
+  let count = 0;
+  unionParent(n1, n2);
+  map.forEach((v, k) => {
+    if (equalParent(v, n2)) count++;
+  });
+  result += count + "\n";
+});
+console.log(result.trimEnd());
