cf9a262c152e1fb9c69bfa44861f454b5497a0a1
Mins97 <tkarnrwl7862@naver.com>
2022-05-18 14:50:33 +0900

    datilay backup

--- backjoon/testing/test.ts
+++ backjoon/testing/test.ts
@@ -1,3 +1,24 @@
 const fs = require("fs");
 const inputList: string[] = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
-const input = inputList[1].split(" ").map(Number);
+
+const queue: string[] = [];
+inputList.slice(1).map((item) => {
+  const [cmd, num] = item.trimEnd().split(" ");
+  switch (cmd) {
+    case "push":
+      queue.push(num);
+      break;
+    case "pop":
+      console.log(queue.shift());
+
+      break;
+    case "size":
+      break;
+    case "empty":
+      break;
+    case "front":
+      break;
+    default: //back
+      break;
+  }
+});
