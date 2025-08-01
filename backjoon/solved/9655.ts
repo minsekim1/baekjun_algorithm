659beccc3d07ba72b952e54764b67b924536644e
minsekim1 <tkarnrwl7862@naver.com>
2025-08-01 17:21:24 +0900

    update new start

---
+++ backjoon/solved/9655.ts
@@ -0,0 +1,13 @@
+// 백준에서 표준 입력 처리용
+import * as fs from "fs";
+
+// 로컬 테스트 시: "input.txt"
+// 백준 제출 시: "/dev/stdin"
+const input = fs.readFileSync("/dev/stdin", "utf-8").trim().split("\n");
+
+// 입력 파싱 예시
+const n = Number(input[0]);
+const arr = input[0].split(" ").map(Number);
+
+// 출력 예시
+console.log(n % 2 === 0 ? "CY" : "SK");
